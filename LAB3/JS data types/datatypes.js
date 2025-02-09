f09a48d6d2a3fa63d2f6b5f83c3dcc1b558ff290
dastan130903 <92976218+dastan130903@users.noreply.github.com>
2025-02-09 19:33:41 +0500

    Create datatypes.js

---
+++ LAB3/JS data types/datatypes.js
@@ -0,0 +1,35 @@
+
+let message = "Hello, World!"; // String
+let count = 42; // Number
+let isAvailable = true; // Boolean
+
+let a = 5;
+let b = 10;
+console.log(a + b); // 15
+
+let firstName = "John";
+let lastName = "Doe";
+console.log(`Hello, ${firstName} ${lastName}!`);
+
+let isOnline = true;
+if (isOnline) {
+  console.log("User is online.");
+} else {
+  console.log("User is offline.");
+}
+
+let value = null;
+console.log(value); // null
+
+let notAssigned;
+console.log(notAssigned); // undefined
+
+let user = {
+  name: "John",
+  age: 30
+};
+console.log(user.name); // John
+
+
+
+
